9952ab72c16c31dd29372a03b599697deb13ba89
EPguitars <pixel122@rambler.ru>
2024-02-11 02:37:07 +0700

    added authentication

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import logging
 import time
 import threading
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException, status
 from pydantic import BaseModel
 import uvicorn
 from rich import print
@@ -16,7 +16,15 @@ class CustomReport(BaseModel):
 
 
 @app.post("/get_proxy")
-async def get_proxy(shop: CustomReport):
+async def get_proxy(shop: CustomReport, password: str):
+    correct_password = "5KW0sjYDYhMCevl"
+    
+    if password != correct_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     
     return proxy_storage[shop.shop].get_proxy()
 
